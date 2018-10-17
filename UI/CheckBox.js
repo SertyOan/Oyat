fe6727d2ca76df8bc1c399a1f3fbd7fc6bba07c0
SertyOan <serty.oan@osyra.net>
2018-10-17 15:11:58 +0200

    review checkbox

--- UI/CheckBox.js
+++ UI/CheckBox.js
@@ -5,6 +5,7 @@ import './CheckBox.css';
 var CheckBox = View.extend({
     __construct: function(options) {
         this.__parent();
+        this.checked = false;
         this.options = {
             text: false,
             defaultChecked: false
@@ -15,34 +16,35 @@ var CheckBox = View.extend({
     setOptions: function(options) {
         this.__parent(options);
 
+        this.checked = this.options.defaultChecked === true;
+
         Helpers.Element.empty(this.elements.root);
 
-        this.elements.input = this.elements.body.appendChild(Helpers.Element.create('input', {
-            type: 'checkbox',
-            className: 'oyat-input',
-            checked: this.options.defaultChecked
+        this.elements.input = this.elements.body.appendChild(Helpers.Element.create('div', {
+            className: 'oyat-input'
         }));
 
         this.elements.input.addEventListener('click', function() {
-            this.emit(this.elements.input.checked ? 'Check' : 'Uncheck')
+            this.checked = !this.checked;
+            this.emit(this.checked ? 'Check' : 'Uncheck')
         }.bind(this));
 
         if (this.options.text) {
-            this.elements.body.appendChild(Helpers.Element.create('span', {
+            this.elements.body.appendChild(Helpers.Element.create('div', {
                 className: 'oyat-text',
                 text: this.options.text
             }));
         }
     },
     isChecked: function() {
-        return this.elements.input.checked;
+        return this.checked;
     },
     check: function() {
-        this.elements.input.checked = true;
+        this.checked = true;
         this.emit('Check');
     },
     uncheck: function() {
-        this.elements.input.checked = false;
+        this.checked = false;
         this.emit('Uncheck');
     }
 });
